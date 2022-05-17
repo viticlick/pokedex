f8fbfadd3175604a88ff3052d1a44a72a6332842
Víctor López <victor@barkibu.com>
2022-05-17 13:13:58 +0200

    Add springboot starting code

---
+++ src/main/java/eu/victorlopez/viticlickpokedex/ViticlickPokedexApplication.java
@@ -0,0 +1,13 @@
+package eu.victorlopez.viticlickpokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViticlickPokedexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViticlickPokedexApplication.class, args);
+    }
+
+}
