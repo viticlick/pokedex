1f416ec766f31525db39af1412b764ffbd14a189
Víctor López <victor@barkibu.com>
2022-05-17 23:40:02 +0200

    create pokemon view

--- src/main/java/eu/victorlopez/viticlickpokedex/controller/PokedexController.java
+++ src/main/java/eu/victorlopez/viticlickpokedex/controller/PokedexController.java
@@ -2,10 +2,13 @@ package eu.victorlopez.viticlickpokedex.controller;
 
 import com.github.oscar0812.pokeapi.models.pokemon.Pokemon;
 import eu.victorlopez.viticlickpokedex.repository.PokemonDBRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
@@ -21,6 +24,15 @@ public class PokedexController {
     public ModelAndView index() {
         List<Pokemon> pokemonList = repository.getPokemonList();
         return new ModelAndView("index", "pokemons", pokemonList);
+    }
+
+    @RequestMapping("/pokemon/{id}")
+    public ModelAndView pokemon(@PathVariable("id") Integer id) {
+        Pokemon pokemon = repository.getById(id);
+        if (pokemon == null) {
+            return new ModelAndView("404", HttpStatus.NOT_FOUND);
+        }
+        return new ModelAndView("pokemon", "pokemon", pokemon);
     }
 
 }
