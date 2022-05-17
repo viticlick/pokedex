a36c3e5c5c0fc22d339a462c5e1aaac9266fbe54
Víctor López <victor@barkibu.com>
2022-05-17 22:24:20 +0200

    List pokemons by name

--- src/main/java/eu/victorlopez/viticlickpokedex/controller/PokedexController.java
+++ src/main/java/eu/victorlopez/viticlickpokedex/controller/PokedexController.java
@@ -1,13 +1,18 @@
 package eu.victorlopez.viticlickpokedex.controller;
 
+import com.github.oscar0812.pokeapi.models.resources.NamedAPIResourceList;
+import com.github.oscar0812.pokeapi.utils.Client;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PokedexController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        NamedAPIResourceList pokemonList = Client.getPokemonList(151, 0);
+        return new ModelAndView("index", "pokemons", pokemonList.getResults());
     }
+
 }
